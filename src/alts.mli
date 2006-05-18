8a0b5d19dfc64f99b7ec45e922699a69d8450f80
dboulytchev <dboulytchev@gmail.com>
2006-05-18 16:26:44 +0000

    Alts hierarchy by S.Galanov committed

--- src/alts.mli
+++ src/alts.mli
@@ -1,6 +1,6 @@
 (*
  * Alts: alt finding algorithms.
- * Copyright (C) 2005
+ * Copyright (C) 2005, 2006
  * Sergey Galanov, St.Petersburg State University
  * 
  * This software is free software; you can redistribute it and/or
@@ -12,34 +12,55 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * 
  * See the GNU Library General Public License version 2 for more details
- * (enclosed in the file LGPL).
+ * (enclosed in the file COPYING).
  *)
 
 (** {1 Alt (single-entry region) construction and manipulation} *)
 
 (** Alt finding constructor *)
 module Make (D: DFST.Sig) : 
-	sig
+  sig
 
-		(** The DFST module *)
-		module T : DFST.Sig with module G = D.G
+    (** The DFST module *)
+    module T : DFST.Sig with module G = D.G
 
-		(** The graph module *)
-		module G : Digraph.Sig with type t = D.G.t and module Node = D.G.Node and module Edge = D.G.Edge
+    (** The graph module *)
+    module G : Digraph.Sig with type t = D.G.t and module Node = D.G.Node and module Edge = D.G.Edge
 
-		(** The graph data *)
-		val graph : G.t
+    (** The graph data *)
+    val graph : G.t
 
-		(** The starting node *)
-		val start : G.Node.t
+    (** The starting node *)
+    val start : G.Node.t
 
-		(** Maximal alt finding module *)
-		module MA :
-			sig
+    (** Maximal alt module type *)
+    module type MA = 
+      sig
 
-				(** [get node] gets maximal alt with header [node] *)
-				val get : G.Node.t -> G.Node.t list
+        (** [get node] gets maximal alt with header [node] using default hooks *)
+	val get : G.Node.t -> G.Node.t list
 
-			end
+      end
 
-	end
+    (** Basic algorithm for finding maximal alt *)
+    module MA : MA
+
+    (** Maximal alts hierarchy finding module *)
+    module HIER : 
+      sig
+
+        (** Dominance tree *)
+        module Tree : Tree.Tree with type t = G.Node.t
+
+        (** Maximal alt *)
+        module MA : MA
+
+        (** Hierarchy printer *)
+        val toDOT : unit -> string
+
+      end
+
+    (** Create a maximal alt module from dominance tree *)
+    module MAFromDom (T: Tree.Tree with type t = G.Node.t) : MA
+
+  end
