76ae7be682ee1626dce8c555cc24f35283d8ed5e
grisxa <grisxa@gmail.com>
2009-04-22 12:49:23 +0000

    Inside local networks you could use a proxy server to download ocamlgraph

--- get-ocamlgraph.sh
+++ get-ocamlgraph.sh
@@ -10,6 +10,7 @@ test -d "$1" && cp -f m4/build.sh "$1/" && exit
 
 test -d ../"$NAME-$VERSION" && mv ../"$NAME-$VERSION" "$1" && cp -f m4/build.sh "$1/" && exit
 
+# export http_proxy=http://proxy:3128/
 test -f ../"$NAME-$VERSION.$EXT" || wget -O ../"$NAME-$VERSION.$EXT" \
 	"http://ocamlgraph.lri.fr/download/$NAME-$VERSION.$EXT"
 
